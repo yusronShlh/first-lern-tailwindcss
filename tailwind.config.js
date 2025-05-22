d294e574a6615ab5c86f70ff0a361d6aded05c6e
Yusron Sholeh <yusron.sholeh12@gmail.com>
2025-05-22 22:28:16 +0700

    updates my studying

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        bgg: "url('/assets/bgg.png')",
+      },
+      fontFamily: {
+        garamond: ['"EB Garamond"', "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
